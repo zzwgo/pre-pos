ea320643e21b73301d1729434916d9228c9f4e65
zzw <33030726+zzwgo@users.noreply.github.com>
2019-07-09 10:58:40 +0800

    Update practice-3.js

--- practice/section-2/practice-3.js
+++ practice/section-2/practice-3.js
@@ -1,5 +1,22 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  var countObjs = {};
+  collection.reduce(function(obj, curr) {
+    if(curr.length === 1) {
+      obj[curr]? obj[curr]++: obj[curr] = 1;
+    }else {
+      var currArr = curr.split(/\W+/);
+      obj[currArr[0]]? obj[currArr[0]] += parseInt(currArr[1]): obj[currArr[0]] = parseInt(currArr[1]);
+    }
+    return obj;
+  }, countObjs);
+  var result = [];
+  for(let key in countObjs) {
+    let obj = {};
+    obj['name'] = key;
+    obj['summary'] = countObjs[key];
+    result.push(obj);
+  }
+  return result;
 }
